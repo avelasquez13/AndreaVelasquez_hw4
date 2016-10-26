21dad4e10637cc994e6b3e50673a3cb71f245d90
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2016-10-25 20:17:34 -0500

    funciona

--- punto_2/animacion.py
+++ punto_2/animacion.py
@@ -1,1 +1,26 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import os
+
+f = open('valores.dat', 'r')
+listu = f.readlines()
+f.close()
+
+arrayu = np.array(listu)
+
+tf = 200
+u = np.array_split(arrayu, tf)
+
+
+for i in range(tf):
+
+    plt.figure()
+    plt.plot(u[i])
+    plt.ylim(0, 1)
+    name = 'u_%05d.png' %i
+    plt.savefig(name)
+    plt.close()
+
+os.system('convert -delay 10 -loop 0 *png cuerda.gif')
+os.system('rm *.png')
